4cecec2a61a719a262066f6be0441d02be4cd6f0
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-01-13 14:13:23 -0300

    repositories added

---
+++ src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.picpaysimplificado.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.picpaysimplificado.domain.transaction.Transaction;
+
+public interface TransactionRepository extends JpaRepository< Transaction, Long > {
+    public List<Transaction> findTransactionsByUserId(Long userId);
+}
